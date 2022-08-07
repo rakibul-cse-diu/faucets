9a9bb37ac25604beb557db4149fd7314b45c950e
rakibul-cse-diu <rakibul2180@gmail.com>
2022-08-07 15:24:13 +0600

    create theme

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2B86C5",
+          "secondary": "#0ABBBF",
+          "accent": "#2D2D2D",
+          "neutral": "#707070",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
